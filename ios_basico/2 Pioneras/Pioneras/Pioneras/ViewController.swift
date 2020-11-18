bcf48b2de5f266380ca4d9daaf8972044e193712
yasmanets <yaser.94ae@gmail.com>
2020-11-19 00:53:13 +0100

    versión 1

---
+++ ios_basico/2 Pioneras/Pioneras/Pioneras/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Pioneras
+//
+//  Created by Yaser  on 19/11/2020.
+//  Copyright © 2020 Yaser . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func retornoDeSecundario(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
